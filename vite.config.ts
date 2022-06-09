c57d09341f95eee0005de6801f8d49117afcc423
Johannes Palvanen <johannes.palvanen@seriously.com>
2022-06-09 16:34:11 +0300

    Update vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import checker from "vite-plugin-checker";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), checker({ typescript: true })]
-})
+    plugins: [react(), tsconfigPaths(), checker({ typescript: true })],
+});
