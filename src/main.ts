d06c44646c8c57c34f52400c80123d148d11d6e1
Johannes Palvanen <johku@Johanness-MacBook-Pro.local>
2024-12-29 22:42:18 +0200

    Implement experience cards and header shapes

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
-// Nothing to do here for now.
+class TimeSince extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = this.getYearsAndMonthsSince(this.innerHTML);
+    }
+
+    private getYearsAndMonthsSince(dateString: string) {
+        const [day, month, year] = dateString.split("-").map(Number);
+        const inputDate = new Date(year, month - 1, day);
+        const currentDate = new Date();
+    
+        let years = currentDate.getFullYear() - inputDate.getFullYear();
+        let months = currentDate.getMonth() - inputDate.getMonth();
+    
+        if (months < 0) {
+            years--;
+            months += 12;
+        }
+    
+        return `${years}y ${months}mo`;
+    }
+}
+
+customElements.define("time-since", TimeSince)
