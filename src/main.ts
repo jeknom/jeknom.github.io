d2a1765f5739ba369ebd4308a5cc274102cb7f4f
Johannes Palvanen <johku@Johanness-MacBook-Pro.local>
2024-12-30 11:08:16 +0200

    Simplify heading styles - Add default appear animation for img elements

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const imgElements = document.querySelectorAll("img");
+
+    imgElements.forEach((imgEl) => {
+        imgEl.addEventListener(
+            "load",
+            () => imgEl.classList.add("animate-appear"),
+        );
+
+        // If the image is already loaded (cached by the browser), add the class
+        if (imgEl.complete) {
+            imgEl.classList.add("animate-appear");
+        }
+    });
+});
+
 class TimeSince extends HTMLElement {
     connectedCallback() {
         this.innerHTML = this.getYearsAndMonthsSince(this.innerHTML);
@@ -7,17 +23,17 @@ class TimeSince extends HTMLElement {
         const [day, month, year] = dateString.split("-").map(Number);
         const inputDate = new Date(year, month - 1, day);
         const currentDate = new Date();
-    
+
         let years = currentDate.getFullYear() - inputDate.getFullYear();
         let months = currentDate.getMonth() - inputDate.getMonth();
-    
+
         if (months < 0) {
             years--;
             months += 12;
         }
-    
+
         return `${years}y ${months}mo`;
     }
 }
 
-customElements.define("time-since", TimeSince)
+customElements.define("time-since", TimeSince);
